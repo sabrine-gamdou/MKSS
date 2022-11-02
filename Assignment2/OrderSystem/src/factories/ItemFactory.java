ab31e6466ad42510c231c844fea4ed89ad2355f4
LauraNeu <lschuppenhauer@stud.hs-bremen.de>
2022-11-02 16:13:52 +0100

    Add comments and delete zip file

--- Assignment2/OrderSystem/src/factories/ItemFactory.java
+++ Assignment2/OrderSystem/src/factories/ItemFactory.java
@@ -7,16 +7,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 /**
- * This is the common interface for items family
+ * Abstract factory knows about all item types
  */
 public interface ItemFactory {
 
+    // Methods from old Order class
     List<Item> getItems();
     void setCheckoutTime(LocalDateTime checkoutTime);
     LocalDateTime getCheckoutTime();
     int getSum();
     void setSum(int sum);
 
+    // Methods for creating products and services
     void createProduct(ItemFactory itemFactory);
     void createService(ItemFactory itemFactory);
 }
