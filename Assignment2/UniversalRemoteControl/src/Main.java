7f5c3ec200d42609da801b0d16a815603758df30
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-03 21:08:27 +0100

    Tested the functionality of the application

--- Assignment2/UniversalRemoteControl/src/Main.java
+++ Assignment2/UniversalRemoteControl/src/Main.java
@@ -1,24 +1,39 @@
+import commands.*;
 import remotecontrol.RemoteControl;
 
 /**
  * Main for testing the functionality
  */
 public class Main {
-	public static void main(String[] args) {
-		RemoteControl mediaPlayerRemote = new RemoteControl();
+    public static void main(String[] args) {
+        RemoteControl mediaPlayerRemote = new RemoteControl();
 
-		// TODO: configure the remote controls for the media player remote
+        mediaPlayerRemote.configureButtonAction(0, new OnCommand(), new OffCommand());
+        mediaPlayerRemote.configureButtonAction(1, new NextCommand(), new PreviousCommand());
+        mediaPlayerRemote.configureButtonAction(2, new PlayCommand(), new PauseCommand());
 
-		// TODO: test the functionality by pressing different buttons similar to below
-		mediaPlayerRemote.actionButtonPressed(0);
-		mediaPlayerRemote.actionButtonPressed(1);
+        System.out.println("\nPressing buttons: ");
+        mediaPlayerRemote.actionButtonPressed(0);
+        mediaPlayerRemote.actionButtonPressed(1);
+        mediaPlayerRemote.actionButtonPressed(2);
+
+        mediaPlayerRemote.undoButtonPressed();
+
+        mediaPlayerRemote.actionButtonPressed(1);
+        mediaPlayerRemote.undoButtonPressed();
+
+        mediaPlayerRemote.actionButtonPressed(0);
+        mediaPlayerRemote.undoButtonPressed();
+        mediaPlayerRemote.undoButtonPressed();
+
 		mediaPlayerRemote.actionButtonPressed(2);
-		mediaPlayerRemote.undoButtonPressed();
-
+		mediaPlayerRemote.actionButtonPressed(2);
 		mediaPlayerRemote.actionButtonPressed(1);
-		mediaPlayerRemote.undoButtonPressed();
 
-		mediaPlayerRemote.actionButtonPressed(0);
-	}
+		mediaPlayerRemote.undoButtonPressed();
+		mediaPlayerRemote.undoButtonPressed();
+		mediaPlayerRemote.undoButtonPressed();
+		mediaPlayerRemote.undoButtonPressed();
+    }
 }
 
