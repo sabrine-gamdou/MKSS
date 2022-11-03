2bd19368ede269ef43883cf03b96caa484a26ca5
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-03 21:08:12 +0100

    Implemented button action configuration

--- Assignment2/UniversalRemoteControl/src/remotecontrol/IRemoteControl.java
+++ Assignment2/UniversalRemoteControl/src/remotecontrol/IRemoteControl.java
@@ -1,25 +1,31 @@
 package remotecontrol;
 
+import commands.Command;
+
 /**
  * Interface for a generic remote control.
  */
 public interface IRemoteControl {
 
-    // TODO: Method for configuration of action buttons
-
-    public void configureButtonAction();
-
+    /**
+     * Configures the given button number with the given opposite commands (For example: configure button 0 with
+     * the positive command PLAY and the negative command PAUSE
+     * @param buttonNo number of the button to be configured
+     * @param positiveCommand the positive command of the given button
+     * @param negativeCommand the negative command of the given button
+     */
+    void configureButtonAction(int buttonNo, Command positiveCommand, Command negativeCommand);
     /**
      * The action button was pressed.
      * Depending on its status, it will execute an activate or deactivate action.
      *
      * @param no The number of the button.
      */
-    public void actionButtonPressed(int no);
+    void actionButtonPressed(int no);
 
     /**
      * The undo button was pressed.
      * It will undo the previous action.
      */
-    public void undoButtonPressed();
+    void undoButtonPressed();
 }
--- Assignment2/UniversalRemoteControl/src/remotecontrol/RemoteControl.java
+++ Assignment2/UniversalRemoteControl/src/remotecontrol/RemoteControl.java
@@ -1,63 +1,91 @@
 package remotecontrol;
 
-import java.util.ArrayList;
-import java.util.List;
+import commands.Command;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 /**
- *  Base implementation for the remote control.
+ * Base implementation for the remote control.
  */
 public class RemoteControl implements IRemoteControl {
 
-		public final static int NO_OF_ACTION_BUTTONS = 3;
+    public final static int NO_OF_ACTION_BUTTONS = 3;
+    private final boolean[] buttonStatus;
+    private final Map<Integer, Map.Entry<Command, Command>> configuredActions;
+    private final Stack<Command> commandHistory;
 
-		private boolean[] buttonStatus;
-		// TODO: Data structures for the actions
-		private List<String> actions;
-		public RemoteControl() {
-			buttonStatus = new boolean[NO_OF_ACTION_BUTTONS];
+    public RemoteControl() {
+        buttonStatus = new boolean[NO_OF_ACTION_BUTTONS];
+        configuredActions = new HashMap<>();
+        commandHistory = new Stack<>();
+    }
 
-			// TODO: Initialize data structures for the actions
-			actions = new ArrayList<>();
-			actions.add("on/off");
-			actions.add("play/pause");
-			actions.add("next/previous");
-		}
+    public void configureButtonAction(int buttonNumber, Command positiveCommand, Command negativeCommand) {
+        System.out.printf("\nButton %s configuration: %n", buttonNumber);
+        System.out.printf(">>Positive command: %s%n", positiveCommand);
+        System.out.printf(">>Negative command: %s%n", negativeCommand);
 
-		// TODO: Implement method for configuration of action buttons
+        configuredActions.put(buttonNumber, new Map.Entry<>() {
+            @Override
+            public Command getKey() {
+                return positiveCommand;
+            }
 
-	@Override
-	public void configureButtonAction() {
+            @Override
+            public Command getValue() {
+                return negativeCommand;
+            }
 
-	}
+            @Override
+            public Command setValue(Command value) {
+                return value;
+            }
+        });
+    }
 
-	/**
-		 * The action button was pressed.
-		 * Depending on its status, it will execute an activate or deactivate action.
-		 *
-		 * @param no The number of the button.
-		 */
-		public void actionButtonPressed(int no) {
-			// Execute action
-			if (buttonStatus[no] == false) {
-				System.out.println("Button activated: " + no);
-				// TODO: Execute activation action
-				// TODO: Configure undo (deactivation) action
-			} else {
-				System.out.println("Button deactivated: " + no);
-				// TODO: Execute deactivation action
-				// TODO: Configure undo (activation) action
-			}
-			// Invert button status
-			buttonStatus[no] = !buttonStatus[no];
-		}
+    /**
+     * The action button was pressed.
+     * Depending on its status, it will execute an activate or deactivate action.
+     *
+     * @param no The number of the button.
+     */
+    public void actionButtonPressed(int no) {
+        Command positiveCommand = configuredActions.get(no).getKey();
+        Command negativeCommand = configuredActions.get(no).getValue();
+        // Execute action
+        if (!buttonStatus[no]) {
+            System.out.println("\nButton activated: " + no);
+            positiveCommand.execute();
+            commandHistory.push(positiveCommand);
+            positiveCommand.configureUndo(negativeCommand);
+        } else {
+            System.out.println("\nButton deactivated: " + no);
+            negativeCommand.execute();
+            commandHistory.push(negativeCommand);
+            negativeCommand.configureUndo(positiveCommand);
+        }
+        // Invert button status
+        buttonStatus[no] = !buttonStatus[no];
+    }
 
-		/**
-		 * The undo button was pressed.
-		 * It will undo the previous action.
-		 */
-		public void undoButtonPressed() {
-			// Execute undo action
-			System.out.println("Undo button pressed");
-			// TODO: Execute undo action
-		}
+    /**
+     * The undo button was pressed.
+     * It will undo the previous action.
+     */
+    public void undoButtonPressed() {
+        if(commandHistory.isEmpty()){
+            System.out.println("Empty history, no command to undo.");
+            return;
+        }
+
+        System.out.println("\n<<<<<<<< Undo button pressed\n");
+        Command oppositeCommand = commandHistory.pop().getOppositeCommand();
+
+        if(oppositeCommand != null){
+            commandHistory.push(oppositeCommand);
+            oppositeCommand.execute();
+        }
+    }
 }
