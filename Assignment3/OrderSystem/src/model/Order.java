9be988668f975899efa111d37d919bc1dd315754
LauraNeu <lschuppenhauer@stud.hs-bremen.de>
2022-11-04 17:00:37 +0100

    Delete comments

--- Assignment3/OrderSystem/src/model/Order.java
+++ Assignment3/OrderSystem/src/model/Order.java
@@ -6,27 +6,27 @@ import java.util.List;
 
 public class Order {
 
-//    private List<Item> items = new ArrayList<>();
-//    private LocalDateTime checkoutTime;
-//    private int sum = 0;
-//
-//    public List<Item> getItems() {
-//        return items;
-//    }
-//
-//    public void setCheckoutTime(LocalDateTime checkoutTime) {
-//        this.checkoutTime = checkoutTime;
-//    }
-//
-//    public LocalDateTime getCheckoutTime() {
-//        return checkoutTime;
-//    }
-//
-//    public int getSum() {
-//        return sum;
-//    }
-//
-//    public void setSum(int sum) {
-//        this.sum = sum;
-//    }
+    private List<Item> items = new ArrayList<>();
+    private LocalDateTime checkoutTime;
+    private int sum = 0;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setCheckoutTime(LocalDateTime checkoutTime) {
+        this.checkoutTime = checkoutTime;
+    }
+
+    public LocalDateTime getCheckoutTime() {
+        return checkoutTime;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
 }
