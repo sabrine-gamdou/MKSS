363d33b0fbd649ee377411a2a6596e7ec24ca9c2
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-06 14:17:16 +0100

    Created enum for user input integers

---
+++ Assignment3/OrderSystem/src/ui/UserInput.java
@@ -0,0 +1,15 @@
+package ui;
+
+public enum UserInput {
+    FINISH(0),
+    ADD_PRODUCT(1),
+    ADD_SERVICE(2);
+
+    private final int value;
+
+    UserInput(final int newValue) {
+        value = newValue;
+    }
+
+    public int getValue() { return value; }
+}
