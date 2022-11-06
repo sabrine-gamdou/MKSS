2a1dda877982d4e38b671aa1ef1470a990ddc5b6
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-06 14:17:36 +0100

    Added option to pick UI (GUI or CLI)

--- Assignment3/OrderSystem/src/OrderSystemMain.java
+++ Assignment3/OrderSystem/src/OrderSystemMain.java
@@ -1,9 +1,18 @@
 import factories.SimpleItemFactory;
 import logic.OrderService;
+import ui.CLI;
+import ui.UI;
 
 public class OrderSystemMain {
 	public static void main(String[] args) {
-		OrderService orderService = new OrderService();
+		/*UI userInterface;
+		if(args[0].equals("GUI")){
+			userInterface = new GUI();
+		}else{
+			userInterface = new CLI();
+		}*/
+		UI userInterface = new CLI();
+		OrderService orderService = new OrderService(userInterface);
 		SimpleItemFactory simpleItemFactory = new SimpleItemFactory();
 		orderService.setSimpleItemFactory(simpleItemFactory);
 		orderService.menuLoop();
