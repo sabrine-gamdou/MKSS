d8e6b72dc46315184ee2fc23b924dd30be999826
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-10 02:38:43 +0100

    Updated methods - removed cli input

--- Assignment3/Exercise2/OrderSystem/src/factories/ItemFactory.java
+++ Assignment3/Exercise2/OrderSystem/src/factories/ItemFactory.java
@@ -8,8 +8,7 @@ import model.Service;
  * Abstract factory knows about all item types
  */
 public interface ItemFactory {
-
     // Methods for creating products and services
-    Product createProduct();
-    Service createService();
+    Product createProduct(String productName, int productPrice, int productQuantity);
+    Service createService(String serviceName, int servicePersons, int serviceHours);
 }
--- Assignment3/Exercise2/OrderSystem/src/factories/SimpleItemFactory.java
+++ Assignment3/Exercise2/OrderSystem/src/factories/SimpleItemFactory.java
@@ -1,6 +1,5 @@
 package factories;
 
-import logic.Input;
 import model.Product;
 import model.Service;
 
@@ -8,26 +7,13 @@ import model.Service;
  * This class implements the ItemFactory and creates product and service instances
  */
 public class SimpleItemFactory implements ItemFactory{
-
     @Override
-    public Product createProduct() {
-        System.out.println("Name: ");
-        String productName = Input.readString();
-        System.out.println("Unit price (in cents): ");
-        int productPrice = Input.readInt();
-        System.out.println("Quantity: ");
-        int productQuantity = Input.readInt();
+    public Product createProduct(String productName, int productPrice, int productQuantity) {
         return new Product(productName, productPrice, productQuantity);
     }
 
     @Override
-    public Service createService() {
-        System.out.println("Service type: ");
-        String serviceName = Input.readString();
-        System.out.println("Number of persons: ");
-        int servicePersons = Input.readInt();
-        System.out.println("Hours: ");
-        int serviceHours = Input.readInt();
+    public Service createService(String serviceName, int servicePersons, int serviceHours) {
         return new Service(serviceName, servicePersons, serviceHours);
     }
 }
