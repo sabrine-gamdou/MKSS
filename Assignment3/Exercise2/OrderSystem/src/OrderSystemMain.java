bed270f65663757afbc57b415369805ce985a0ff
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-10 02:41:42 +0100

    Depending on main parameter, either GUI or CLI will be started.

--- Assignment3/Exercise2/OrderSystem/src/OrderSystemMain.java
+++ Assignment3/Exercise2/OrderSystem/src/OrderSystemMain.java
@@ -1,20 +1,23 @@
+import controller.OrderController;
 import factories.SimpleItemFactory;
 import logic.OrderService;
-import ui.gui.GUI;
-import ui.UI;
+import repositories.OrderRepositoryImpl;
+import ui.cli.CLI;
 
 public class OrderSystemMain {
-	public static void main(String[] args) {
-		/*UI userInterface;
-		if(args[0].equals("GUI")){
-			userInterface = new GUI();
-		}else{
-			userInterface = new CLI();
-		}*/
-		UI userInterface = new GUI();
-		OrderService orderService = new OrderService(userInterface);
-		SimpleItemFactory simpleItemFactory = new SimpleItemFactory();
-		orderService.setSimpleItemFactory(simpleItemFactory);
-		orderService.menuLoop();
-	}
+    public static void main(String[] args) {
+        OrderService orderService = new OrderService();
+        // Initialize the components in the OrderService
+        orderService.setOrderRepository(new OrderRepositoryImpl());
+        orderService.setSimpleItemFactory(new SimpleItemFactory());
+        orderService.initializeService();
+
+        if (args.length > 0 && args[0].equals("GUI")) {
+            OrderController orderController = new OrderController(orderService);
+            orderController.initializeController();
+        } else {
+            CLI cli = new CLI(orderService);
+            cli.menuLoop();
+        }
+    }
 }
