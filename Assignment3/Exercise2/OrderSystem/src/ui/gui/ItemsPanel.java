42cc7233941d7adb48de96b69ed501606d370b3d
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-10 02:28:05 +0100

    Added dialogs and panels for the items and the orders

---
+++ Assignment3/Exercise2/OrderSystem/src/ui/gui/ItemsPanel.java
@@ -0,0 +1,54 @@
+package ui.gui;
+
+import model.Item;
+import model.Order;
+import utils.Utility;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
+
+public class ItemsPanel extends JPanel{
+
+    private JTable table;
+    private DefaultTableModel tableModel;
+
+    public ItemsPanel(JFrame frame, String itemName, String itemPriceLabel, String itemDescriptionLabel) {
+        initializeContent(new String[]{itemName, itemPriceLabel, itemDescriptionLabel});
+        JPanel panelS = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        add(panelS, BorderLayout.SOUTH);
+    }
+
+    private void initializeContent(String[] header){
+        table = new JTable();
+        tableModel = new DefaultTableModel(header,0);
+        table.setModel(tableModel);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(10, 304, 500, 180);
+
+        add(scrollPane);
+        setUpTableData(null);
+    }
+
+    public void updateTable(Order currentOrder) {
+        setUpTableData(currentOrder);
+    }
+
+
+    private void setUpTableData(Order currentOrder) {
+        if(currentOrder != null && !currentOrder.getItems().isEmpty()) {
+            tableModel.setRowCount(0);
+            Utility.sortItems(currentOrder);
+            List<Item> items = currentOrder.getItems();
+            for (Item item : items) {
+                tableModel.addRow(new Object[]{item.getName(), Utility.formatPrice(item.getPrice()), item.toString()});
+            }
+            table.setModel(tableModel);
+            tableModel.fireTableDataChanged();
+        }else{
+            tableModel.setRowCount(0);
+        }
+    }
+}
