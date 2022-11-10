7997a1b079e72d92eefd32b99e7c7231665b4e16
sabrine-gamdou <47325924+sabrine-gamdou@users.noreply.github.com>
2022-11-10 02:40:21 +0100

    Added controller for the GUI

--- Assignment3/Exercise2/OrderSystem/src/controller/OrderController.java
+++ Assignment3/Exercise2/OrderSystem/src/controller/OrderController.java
@@ -1,20 +1,77 @@
 package controller;
 
+import model.Order;
+import ui.OrderServiceInterface;
 import ui.gui.GUI;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class OrderController implements ActionListener {
-    private GUI gui;
+public class OrderController {
+    private GUI view;
+    private OrderServiceInterface model;
 
-
-    public OrderController() {
-        this.gui = new GUI();
+    public OrderController(OrderServiceInterface orderServiceInterface) {
+        this.model = orderServiceInterface;
+        this.view = new GUI();
+        initializeView();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    private void initializeView(){
+        getCurrentOrder();
+    }
 
+    public OrderServiceInterface getModel() {
+        return model;
+    }
+
+    public void initializeController() {
+        // Set action listener to product dialog buttons
+        view.getNewProductDialog().getSaveButton().addActionListener(e -> {
+            addProduct();
+            getCurrentOrder();
+            view.getNewProductDialog().setVisible(false);
+        });
+        view.getNewProductDialog().getQuitButton().addActionListener(e -> view.getNewProductDialog().setVisible(false));
+
+        // Set action listener to service dialog buttons
+        view.getNewServiceDialog().getSaveButton().addActionListener(e -> {
+            addService();
+            getCurrentOrder();
+            view.getNewServiceDialog().setVisible(false);
+        });
+        view.getNewServiceDialog().getQuitButton().addActionListener(e ->  view.getNewServiceDialog().setVisible(false));
+
+        // Set action listener to finish dialog buttons
+        view.getFinishButton().addActionListener(this::showFinishedOrder);
+        view.getNewClientFinishDialog().getQuitButton().addActionListener(e -> resetOrder());
+    }
+
+    private void addProduct() {
+        model.addProduct(view.getNewProductDialog().getItemName(),
+                        view.getNewProductDialog().getItemsPriceLabel(),
+                        view.getNewProductDialog().getQuantityLabel());
+    }
+
+    private void addService() {
+        model.addService(view.getNewServiceDialog().getItemName(),
+                        view.getNewServiceDialog().getItemsPriceLabel(),
+                        view.getNewServiceDialog().getQuantityLabel());
+    }
+
+    private void resetOrder() {
+        model.initializeService();
+        getCurrentOrder();
+    }
+
+    private Order finishOrder() {
+        return model.finishOrder(); //got finished order from backend -- need to render and update GUI -- update order in view
+    }
+
+    private void getCurrentOrder() {
+        view.getItemsPanel().updateTable(model.getCurrentOrder()); //got order from backend -- need to render and update GUI
+    }
+
+    private void showFinishedOrder(ActionEvent e) {
+        view.getNewClientFinishDialog().showDialog(finishOrder());
     }
 }
